3dee369dadae79683d5a31422a3b4168d58284d4
andrew <Toggledevelopment@gmail.com>
2020-12-30 22:01:22 -0800

    pulling from main merge

--- ToggleDev2/Controller/ImagePickerVC.swift
+++ ToggleDev2/Controller/ImagePickerVC.swift
@@ -7,14 +7,16 @@
 
 import Foundation
 import UIKit
+import AVKit
 
 class ImagePickerVC: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    var videoURL: NSURL?
-    @IBOutlet weak var video: UIImageView!
+    var videoURL: URL?
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+    @IBOutlet weak var video: UIImageView!
     @IBAction func buttonTapped(_ sender: UIButton) {
         
         let imagePickerVC = UIImagePickerController()
@@ -25,14 +27,40 @@ class ImagePickerVC: UIViewController , UIImagePickerControllerDelegate, UINavig
     }
     
 
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             // do someting...
             picker.dismiss(animated: true, completion: nil)
             
-            guard let videoURL = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerReferenceURL")] as? NSURL else {return}
-            self.videoURL = videoURL
-            print(videoURL)
+            
+           // if let image = info[.mediaType] as? UIImage {
+             //   video.image = image
+               // }
+            if let videoURL = info[.mediaURL] as? URL {
+                self.videoURL=videoURL
+                print(videoURL)
+            }
+                let asset = AVAsset(url: videoURL!)
+                let assetImageGenerator = AVAssetImageGenerator(asset: asset)
+
+                var time = asset.duration
+                time.value = min(time.value, 2)
+
+                do {
+                    let imageRef = try assetImageGenerator.copyCGImage(at: time, actualTime: nil)
+                    video.image = UIImage(cgImage: imageRef)
+                } catch {
+                    print("error")
+                    
+                }
+            
+            
         }
+    
+    @IBAction func submitPost(_ sender: Any) {
+        
+    }
+    
+    
     
     
     
